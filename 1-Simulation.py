52004c26c4f32e7160515d57d16da6c44e44e43a
johnwoodill <johnwoodill@gmail.com>
2020-01-29 18:55:30 -0800

    finished speed and calibration

--- 1-Simulation.py
+++ 1-Simulation.py
@@ -8,6 +8,25 @@ from math import acos, degrees
 import scipy.stats
 
 
+# Constants
+NAGENTS = 10     # number of agents
+NTIME = 720    # number of time steps 24-hours * 30 days
+IUU_EVENT = 312   # Time of illegal event
+#v = 0.0625        # velocity 
+tspeed = 0.025    # Traveling speed
+ispeed = 0.025    # Speed when close to IUU
+fspeed = 0.0025   # Fishing speed
+#iuuv = 0.0625
+
+e = 0.001          # separation error
+ie = 0.25         # IUU separation error
+iee = 0.1         # separation error from other vessels if alert
+FA_X1 = 0.6       # Fishing area coords.
+FA_X2 = 0.8
+FA_Y1 = 0.2
+FA_Y2 = 0.8
+
+
 def dist(x1, x2, y1, y2):
     return ( np.sqrt( (x2 - x1)**2 + (y2 - y1)**2) )
 
@@ -16,26 +35,20 @@ def dist(x1, x2, y1, y2):
 # x_c = x_a - (d_2 * (x_a - x_b))  /d
 def solve_dist(x1, x2, y1, y2, max_speed):
     d = dist(x1, x2, y1, y2)
-    d2 = max_speed
-    x3 = x1 - ( d2 * (x1 - x2)) / (d)
-    y3 = y1 - ( d2 * (y1 - y2)) / (d)
+    if d > max_speed:
+        d2 = max_speed
+        x3 = x1 - ( d2 * (x1 - x2)) / (d)
+        y3 = y1 - ( d2 * (y1 - y2)) / (d)
+    else:
+        x3 = x2
+        y3 = y2
     return x3, y3
 
    
-#x3, y3 = solve_dist(x1, x2, y1, y2, max_dist = 0.25)
-
 def calc_vmove(x1, x2, y1, y2, inverse_dist=False, random_dir=False, max_speed=0.025):
     '''
-    Calculate trig angle and return direction
-    inverse_dist: Inverse distance moves away from target
-    vmult: velocity multiplier 
+    info
     '''
-    # Get distance to target
-    #ldist = dist(x1, x2, y1, y2)
-    #vel = ldist/25
-    #print(f"ldist: {ldist} - vel: {vel}")
-    #vel=1
-
     if random_dir == True:
         # Update location adjusting for sign (inverse)
         randx = np.linspace(x2 - .10, x2 + .10, 50)
@@ -43,25 +56,20 @@ def calc_vmove(x1, x2, y1, y2, inverse_dist=False, random_dir=False, max_speed=0
         x2 = np.random.choice(randx, 1)
         y2 = np.random.choice(randy, 1)
 
-    # More towards target
+    # More away target
     if inverse_dist == True:
         if x1 < x2:
-            x2 = x1 - (x2 - x1)
-        if x1 > x2:
-            x2 = x1 + (x1 - x2)
+            x2 = x1 + (x2 - x1)
+        if x1 >= x2:
+            x2 = x1 - (x1 - x2)
         if y1 < y2:
             y2 = y1 - (y2 - y1)
-        if y1 > y2:
-            y2 = y1 + (y2 - y1)
-        if y1 == y2:
-            y1 = y2
-        if x1 == x2:
-            x1 = x2
+        if y1 >= y2:
+            y2 = y1 + (y1 - y2)
 
     # Calculate movement along linear line
     mx2, my2 = solve_dist(x1, x2, y1, y2, max_speed = max_speed)
     return mx2, my2
-
 
 
 
@@ -139,44 +147,29 @@ def vmove(x1, x2, y1, y2, inverse_dist=False, random_dir=False):
     return mx2, my2
        
 
-     
-# Constants
-NAGENTS = 25     # number of agents
-NTIME = 720       # number of time steps
-IUU_EVENT = 312   # Time of illegal event
-v = 0.0625        # velocity 
-iuuv = 0.0625
-e = 0.01          # separation error
-ie = 0.35         # IUU separation error
-iee = 0.1         # separation error from other vessels if alert
-FA_X1 = 0.6       # Fishing area coords.
-FA_X2 = 0.8
-FA_Y1 = 0.2
-FA_Y2 = 0.8
-
-
 # Define Fishing area
 # x = 0.60, 0.80
 # y = 0.20, 0.80
-farea = list(((x, y) for x in np.linspace(FA_X1, FA_X2, 50) for y in np.linspace(FA_Y1, FA_Y2, 60)))
+farea = list(((x, y) for x in np.linspace(FA_X1, FA_X2, 100) for y in np.linspace(FA_Y1, FA_Y2, 100)))
 
 # Fishing Area
 fxVec = [item[0] for item in farea]
 fyVec = [item[1] for item in farea]
 
 # # Agents in random space
-# xVec = np.mod(np.random.uniform(0, 1, NAGENTS), 1)
-# yVec = np.mod(np.random.uniform(0, 1, NAGENTS), 1)
+xVec = np.mod(np.random.uniform(0, 1, NAGENTS), 1)
+yVec = np.mod(np.random.uniform(0, 1, NAGENTS), 1)
 
 # Agents in random fishing area space
-xVec = np.random.choice(fxVec, NAGENTS)
-yVec = np.random.choice(fyVec, NAGENTS)
+# xVec = np.random.choice(fxVec, NAGENTS)
+# yVec = np.random.choice(fyVec, NAGENTS)
 
 
 
 # Define all vessels as fishing = 0
 agents = pd.DataFrame({'fishing_status': "Traveling",
                              'alert_status': "Fishing",
+                             'fishing_time': 0,
                              'xLoc': xVec,
                              'yLoc': yVec,
                              'fxLoc': random.sample(fxVec, NAGENTS),
@@ -214,7 +207,7 @@ for t in range(NTIME):
     # Introduce IUU Event 
     if t >= IUU_EVENT:
         # Move IUU vessel
-        ix2, iy2 = calc_vmove(ix1, ifx1, iy1, ify1, max_speed = 0.025)
+        ix2, iy2 = calc_vmove(ix1, ifx1, iy1, ify1, max_speed = (tspeed/2))
         
         # Update vessel
         ivessel['xLoc'][0] = ix2
@@ -225,22 +218,25 @@ for t in range(NTIME):
     idat = pd.concat([idat, iindat])
     
     for i in range(NAGENTS):       
+        # Vessel Location
+        x1 = agents['xLoc'][i]
+        y1 = agents['yLoc'][i]
 
         # Fishing location
         fx1 = agents['fxLoc'][i]
         fy1 = agents['fyLoc'][i]
-        
-        # Vessel Location
-        x1 = agents['xLoc'][i]
-        y1 = agents['yLoc'][i]
-        
+               
         # If at location find new fishing location
-        if (dist(x1, fx1, y1, fy1) < e):
-            agents.loc[i, 'fxLoc'] = random.sample(fxVec, 1)[0]
-            agents.loc[i, 'fyLoc'] = random.sample(fyVec, 1)[0]
-            fx1 = agents['fxLoc'][i]
-            fy1 = agents['fyLoc'][i]
-        
+        if (dist(x1, fx1, y1, fy1) <= e):
+            if agents.loc[i, 'fishing_time'] == 10:
+                fx1 = random.sample(fxVec, 1)[0]
+                fy1 = random.sample(fyVec, 1)[0]
+                agents.loc[i, 'fxLoc'] = fx1
+                agents.loc[i, 'fyLoc'] = fy1
+                agents.loc[i, 'fishing_time'] = 0
+            else:
+                agents.loc[i, 'fishing_time'] += 1
+            
         # Calc distances for all vessels
         agents.loc[:, 'dist'] = dist(x1, agents['xLoc'], y1, agents['yLoc'])
         dist_check = agents.sort_values('dist')[1:2]
@@ -248,42 +244,90 @@ for t in range(NTIME):
         dy1 = dist_check['yLoc'].iat[0]
         
         # Get distance to IUU Vessel
-        idist = dist(x1, ix1, y1, iy1)
-                    
-        # Separation:
-        # If close to IUU Vessel move away
-        # if (t >= IUU_EVENT and t <= IUU_EVENT and idist <= ie):
-        if (idist <= ie):
-            x2, y2 = calc_vmove(x1, ix1, y1, iy1, inverse_dist=True, max_speed = 0.025) 
-            agents.loc[i, 'alert_status'] = "Alert"
-
-        # If inside IUU event, far away from iuu, but vessel close to other vessel
-        #elif ( (t > IUU_EVENT) and (t <= (IUU_EVENT + 48)) and (idist >= ie) and (idist < ie + 0.20) and (dist_check['dist'].iat[0] <= iee)):
-        #    x2, y2 = vmove(x1, dx1, y1, dy1, inverse_dist=True, random=True) 
-
-        # If outside second margin of IUU, not close to vessels don't move
-        #elif ( (t > IUU_EVENT) and (t <= (IUU_EVENT + 48)) and (idist >= ie) and (idist < ie + 0.20) and (dist_check['dist'].iat[0] > iee)):
-        #    x2 = x1
-        #    y2 = y1
+        idist = round(dist(x1, ix1, y1, iy1), 2)
         
         # If closests vessel is within error (e) move away from vessel
-        #elif dist_check['dist'].iat[0] <= e:
-        #    x2, y2 = vmove(x1, dx1, y1, dy1, inverse_dist=True) 
+        # if dist_check['dist'].iat[0] <= e:
+        #     x2, y2 = calc_vmove(x1, dx1, y1, dy1, inverse_dist=True, max_speed = tspeed) 
+        #     agents.loc[i, 'alert_status'] = "Case 3"
+
+        # If close to IUU Vessel move away
+        if ( (t >= IUU_EVENT) and (idist < ie) ):
+            x2, y2 = calc_vmove(x1, ix1, y1, iy1, inverse_dist=True, max_speed=ispeed) 
+            agents.loc[i, 'alert_status'] = "Case 1"
         
+        # If outside second margin of IUU don't move
+        if ( (t >= IUU_EVENT) and (idist >= ie) and (idist <= ie + 0.20) ):
+            x2, y2 = calc_vmove(x1, dx1, y1, dy1, inverse_dist=True, max_speed = tspeed, random_dir=True) 
+            #x2 = x1
+            #y2 = y1
+            agents.loc[i, 'alert_status'] = "Case 2"
+       
+        # Otherwise, move towards target fishing area at fishing speed      
+        elif (agents.loc[i, 'fishing_status'] == "Fishing"):
+            x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = fspeed)
+            agents.loc[i, 'alert_status'] = "Case 4"
+        
+        # # Otherwise, move towards target fishing area at travel speed
+        elif (agents.loc[i, 'fishing_status'] == "Traveling"):
+            x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = tspeed)
+            agents.loc[i, 'alert_status'] = "Case 5"
+        # else:
+        #     agents.loc[i, 'alert_status'] = "Case 5"
+
+
+        # # Separation:
+        # if dist_check['dist'].iat[0] <= e:
+        #     x2, y2 = calc_vmove(x1, dx1, y1, dy1, inverse_dist=True, max_speed = tspeed) 
+
+        # # If close to IUU Vessel move away
+        # elif ( (idist <= ie) and (t >= IUU_EVENT) ):
+        #     x2, y2 = calc_vmove(x1, ix1, y1, iy1, inverse_dist=True, max_speed = ispeed) 
+        #     agents.loc[i, 'alert_status'] = "Alert"
+
+        # # If inside IUU event, far away from iuu, but within ie + 0.20 range
+        # elif ( (t > IUU_EVENT) and (t <= (IUU_EVENT + 48)) and (idist > ie + .20)  ):
+        #     x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = tspeed) 
+        
+        # else: 
+        #     x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = tspeed)
+
+        # If outside second margin of IUU, not close to vessels don't move
+        # elif ( (t > IUU_EVENT) and (t <= (IUU_EVENT + 48)) and (idist >= ie) and (idist < ie + 0.20) ):
+        #     x2 = x1
+        #     y2 = y1
+        
+        # If closests vessel is within error (e) move away from vessel
         # Otherwise, move towards target fishing area       
-        else:
-            x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = 0.025)
-            agents.loc[i, 'alert_status'] = "Fishing"
-            
-        # Fishing vs Traveling vs Alert status
-        #if (x2 >= FA_X1 and x2 <= FA_X2 and y2 >= FA_Y1 and y2 <= FA_Y2):
+        #elif ( (agents.loc[i, 'fishing_status'] == 'Fishing') and (agents.loc[i, 'alert_status'] != 'Alert')):
+        #    x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = fspeed)
         #    agents.loc[i, 'fishing_status'] = "Fishing"
-        #else:
-        #    agents.loc[i, 'fishing_status'] = "Traveling"
-            
-        #if (agents['alert_status'][i] == 'Alert'):
-        #    agents.loc[i, 'fishing_status'] = "Alert"
+        #    agents.loc[i, 'alert_status'] = "Not Alert"
         
+        # elif ( (agents.loc[i, 'fishing_status'] == 'Fishing') and (agents.loc[i, 'alert_status'] == 'Alert')):
+        #     x2, y2 = calc_vmove(x1, ix1, y1, iy1, inverse_dist=True,  max_speed = ispeed)
+        #     agents.loc[i, 'fishing_status'] = "Fishing"
+        #     agents.loc[i, 'alert_status'] = "Alert"
+
+        # elif ( (agents.loc[i, 'fishing_status'] == 'Traveling') and (agents.loc[i, 'alert_status'] != 'Alert')):
+        #     x2, y2 = calc_vmove(x1, fx1, y1, fy1, max_speed = tspeed)
+        #     agents.loc[i, 'fishing_status'] = "Traveling"
+        #     agents.loc[i, 'alert_status'] = "Not Alert"
+
+        # elif ( (agents.loc[i, 'fishing_status'] == 'Traveling') and (agents.loc[i, 'alert_status'] == 'Alert')):
+        #     x2, y2 = calc_vmove(x1, ix1, y1, iy1, inverse_dist=True, max_speed = ispeed)
+        #     agents.loc[i, 'fishing_status'] = "Traveling"
+        #     agents.loc[i, 'alert_status'] = "Alert"
+        # Fishing vs Traveling vs Alert status
+        if (x2 >= FA_X1 and x2 <= FA_X2 and y2 >= FA_Y1 and y2 <= FA_Y2):
+           agents.loc[i, 'fishing_status'] = "Fishing"
+        else:
+           agents.loc[i, 'fishing_status'] = "Traveling"       
+        # if (idist >= ie + 0.20):
+        #     agents.loc[i, 'alert_status'] = "Not Alert"
+        # else:
+        #     agents.loc[i, 'alert_status'] = "Alert"
+
         # Move vessel 
         agents.loc[i, 'xLoc'] = x2
         agents.loc[i, 'yLoc'] = y2
